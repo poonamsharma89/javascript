e4ea1f331210604040eb7fa30a2480342be2edec
Poonam Raturi <sharma.poonam89@ymail.com>
2024-09-17 12:17:00 +0000

    control flow code files

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function sam(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
